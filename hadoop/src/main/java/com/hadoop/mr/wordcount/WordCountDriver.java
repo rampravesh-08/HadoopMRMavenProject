472689ecb0da8071c6ba19d2c2137b5cff3164bd
rampravesh-08 <mac.rpravesh.08@outlook.com>
2020-10-23 18:17:11 +0400

    Hadoo map reduce maven project

---
+++ hadoop/src/main/java/com/hadoop/mr/wordcount/WordCountDriver.java
@@ -0,0 +1,34 @@
+package com.hadoop.mr.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountDriver {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf);
+		job.setJarByClass(WordCountDriver.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setNumReduceTasks(1);
+		job.setReducerClass(WordCountReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		FileSystem fs = FileSystem.get(conf);
+		fs.delete(new Path(args[1])); // why are we deleting ?
+		job.waitForCompletion(true);
+	}
+}
---
+++ hadoop/src/main/java/com/hadoop/mr/wordcount/WordCountMapper.java
@@ -0,0 +1,21 @@
+package com.hadoop.mr.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper  extends Mapper<LongWritable, Text, Text, IntWritable> {
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		
+		String[] valArray = value.toString().split(" ");
+		for (String string : valArray) {
+			context.write(new Text(string), new IntWritable(1));
+		}		
+	}
+
+}
